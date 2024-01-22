02abe2f320e2034c5bd62c6e8ba36e90cb732d98
Remofox <Remo.fox@gmail.com>
2024-01-22 18:06:12 +0200

    n

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+ScrollReveal({
+         reset: true,
+        distance:'50px',
+        duration:900,
+         delay:100 
+    });
+      ScrollReveal().reveal('.container h1, .container p,figure', { delay:100 ,origin:'top'});
+      ScrollReveal().reveal('.tt', { delay:100 ,origin:'left'});
